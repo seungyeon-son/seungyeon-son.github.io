5a07ae476444ea2583c6a4517c7204eda3001363
syson <au1gust8@gmail.com>
2024-04-25 00:17:01 +0900

    메인페이지 레이아웃 작업

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Archive from "./pages/archive/ArchivePage";
 import Main from "./pages/main/MainPage";
+import styled from "styled-components";
 
 function Home() {
   return <h2>Home</h2>;
@@ -10,25 +11,60 @@ function Home() {
 function App() {
   return (
     <Router>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/archive">Archive</Link>
-            </li>
-          </ul>
-        </nav>
-
-        <Routes>
-          <Route path="/" element={<Main />} />
-          <Route path="/archive" element={<Archive />} />
-        </Routes>
-      </div>
+      <NavMenu>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/archive">Archive</Link>
+          </li>
+        </ul>
+      </NavMenu>{" "}
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/archive" element={<Archive />} />
+      </Routes>
     </Router>
   );
 }
 
 export default App;
+
+const NavMenu = styled.nav`
+  position: fixed;
+  top: 56px;
+  right: 84px;
+  ul {
+    list-style: none;
+    margin: 0;
+    padding: 0 12px;
+    width: 120px;
+    height: 36px;
+    position: relative;
+    z-index: 1;
+    border-radius: 8px;
+    border: 1px solid #fefefe;
+    overflow: hidden;
+    &::after {
+      display: block;
+      content: "";
+      width: 120px;
+      height: 36px;
+      background: #000;
+      filter: blur(14px);
+      position: absolute;
+      top: 0;
+      left: 0;
+      z-index: -1;
+    }
+    display: flex;
+    align-items: center;
+    gap: 16px;
+    opacity: 0.7;
+  }
+  a {
+    text-decoration: none;
+    color: white;
+  }
+`;
--- src/pages/main/MainPage.jsx
+++ src/pages/main/MainPage.jsx
@@ -2,7 +2,52 @@ import React from "react";
 import styled from "styled-components";
 
 function Main() {
-  return <div>main</div>;
+  return (
+    <PanelWrap>
+      <Panel>
+        <Board>
+          <h1>
+            <p>UI DEVELOPER</p>
+            <p>Published WEB.</p>
+          </h1>
+        </Board>
+      </Panel>
+      <Panel>
+        <Board></Board>
+      </Panel>
+      <Panel></Panel>
+    </PanelWrap>
+  );
 }
 
 export default Main;
+const PanelWrap = styled.div`
+  background: #000;
+`;
+const Board = styled.div`
+  width: calc(100% - 96px);
+  height: calc(100% - 48px);
+  border-radius: 12px;
+  box-sizing: border-box;
+`;
+const Panel = styled.div`
+  height: 100dvh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border: 1px solid orange;
+  ${Board} {
+    &:nth-child(1) {
+      background: #e2e2e2;
+      display: flex;
+      align-items: end;
+      padding: 32px 24px;
+    }
+    &:nth-child(2) {
+      background: #f4efe9;
+    }
+    &:nth-child(3) {
+      background: #ddd;
+    }
+  }
+`;
