1da61cb4a0f0a77a7c8278e42718564dc709fca6
syson <au1gust8@gmail.com>
2024-04-26 01:38:27 +0900

    메인페이지 수정

--- src/pages/main/MainPage.jsx
+++ src/pages/main/MainPage.jsx
@@ -5,15 +5,17 @@ function Main() {
   return (
     <PanelWrap>
       <Panel>
-        <Board>
+        <FileMainShape>
           <h1>
             <p>UI DEVELOPER</p>
             <p>Published WEB.</p>
           </h1>
-        </Board>
+        </FileMainShape>
       </Panel>
       <Panel>
-        <Board></Board>
+        <Board>
+          <FileShape></FileShape>
+        </Board>
       </Panel>
       <Panel></Panel>
     </PanelWrap>
@@ -29,6 +31,25 @@ const Board = styled.div`
   height: calc(100% - 48px);
   border-radius: 12px;
   box-sizing: border-box;
+`;
+const FileShape = styled.div`
+  //TODO: w, h 값 유동 집어넣기
+  width: 100%;
+  height: 500px;
+  background-image: url("../../../../images/file-shape.svg");
+  background-size: contain;
+  background-repeat: no-repeat;
+`;
+const FileMainShape = styled.div`
+  //TODO: w, h 값 유동 집어넣기
+  width: calc(100% - 56px);
+  height: 100%;
+  background-image: url("../../../../images/file-main-shape.svg");
+  background-size: 100% 85%;
+  background-repeat: no-repeat;
+  display: flex;
+  justify-content: start;
+  align-items: end;
 `;
 const Panel = styled.div`
   height: 100dvh;
