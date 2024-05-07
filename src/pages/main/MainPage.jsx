1ae38f39816e8ddd07c5b41b8d9a67f14eb97414
syson <au1gust8@gmail.com>
2024-05-08 02:29:06 +0900

    Design: 메인페이지 수정

--- src/pages/main/MainPage.jsx
+++ src/pages/main/MainPage.jsx
@@ -5,26 +5,23 @@ function Main() {
   return (
     <PanelWrap>
       <Panel>
-        <FileMainShape>
-          <h1>
-            <p>UI DEVELOPER</p>
-            <p>Published WEB.</p>
-          </h1>
-        </FileMainShape>
+        <h1>WEB Publishing / UI Developer</h1>
+        <p>
+          I'm working as a web publisher, and I'm currently studying frontend development. I love thinking about cool
+          designs and bringing them to life. I'm always putting in effort to improve and grow. .
+        </p>
       </Panel>
-      <Panel>
-        <Board>
-          <FileShape></FileShape>
-        </Board>
-      </Panel>
-      <Panel></Panel>
     </PanelWrap>
   );
 }
 
 export default Main;
 const PanelWrap = styled.div`
-  background: #000;
+  background: no-repeat center/100% url("../../../images/main/background.png");
+  background-size: cover;
+  width: 100%;
+  height: 100vh;
+  color: #fff;
 `;
 const Board = styled.div`
   width: calc(100% - 96px);
@@ -32,43 +29,4 @@ const Board = styled.div`
   border-radius: 12px;
   box-sizing: border-box;
 `;
-const FileShape = styled.div`
-  //TODO: w, h 값 유동 집어넣기
-  width: 100%;
-  height: 500px;
-  background-image: url("../../../../images/file-shape.svg");
-  background-size: contain;
-  background-repeat: no-repeat;
-`;
-const FileMainShape = styled.div`
-  //TODO: w, h 값 유동 집어넣기
-  width: calc(100% - 56px);
-  height: 100%;
-  background-image: url("../../../../images/file-main-shape.svg");
-  background-size: 100% 85%;
-  background-repeat: no-repeat;
-  display: flex;
-  justify-content: start;
-  align-items: end;
-`;
-const Panel = styled.div`
-  height: 100dvh;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  border: 1px solid orange;
-  ${Board} {
-    &:nth-child(1) {
-      background: #e2e2e2;
-      display: flex;
-      align-items: end;
-      padding: 32px 24px;
-    }
-    &:nth-child(2) {
-      background: #f4efe9;
-    }
-    &:nth-child(3) {
-      background: #ddd;
-    }
-  }
-`;
+const Panel = styled.div``;
