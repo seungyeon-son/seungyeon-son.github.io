e57d7f5e925b95a49e24788df66366e6af790f6c
syson <au1gust8@gmail.com>
2024-05-29 01:59:10 +0900

    Fix: createRoot 업데이트 React18

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,11 @@
 import React from "react";
+import { createRoot } from "react-dom/client";
 import App from "./App";
-import ReactDOM from "react-dom";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root"),
-);
+const container = document.getElementById("root");
+if (container) {
+  const root = createRoot(container);
+  root.render(<App />);
+} else {
+  console.error("Root element not found");
+}
